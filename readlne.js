c593a196f2112ccca47e30f25d72c1951b459e49
hirwavestine <gilhirwa22@gmail.com>
2019-03-29 16:42:38 +0200

    add comments on realine.js

--- readlne.js
+++ readlne.js
@@ -4,6 +4,7 @@ var RL=readline.createInterface(process.stdin, process.stdout);
 RL.question('What is your name', (name)=>{
     RL.setPrompt(`${name} how old are you?`);
     RL.prompt();
+    //on is a listen and line is an event , built in event
     RL.on('line', (age)=>{
         if(age<18){
             util.log(`${name.trim()} because you are ${age} years old, you can not proceed`);
