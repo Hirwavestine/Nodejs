c8e375b54ff52117dd15d69d5f3c6d6b05fe83b9
hirwavestine <gilhirwa22@gmail.com>
2019-04-04 19:33:57 +0200

    an other way to rename a file and remove in another directory

--- file-renaming.js
+++ file-renaming.js
@@ -1,4 +1,2 @@
 const fs = require("fs");
-
-fs.renameSync();
-// use mv newfile.js newDir/newfile.js in order to move newfile and put it in newDir and reneme newfile2.js
+fs.renameSync("./newfile2.js", "newDir/newfile.js");
