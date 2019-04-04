692f9985b8b0116d2c7aa8d25b7f56439008a756
hirwavestine <gilhirwa22@gmail.com>
2019-04-04 18:38:23 +0200

    removing files and directories

---
+++ file-removing.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+//fs.rmdirSync("./newDir");
+try {
+  //remove a file
+  fs.unlinkSync("./newDir/newFile.js");
+} catch (err) {
+  console.log(err + "here is the error");
+}
