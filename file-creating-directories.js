306312b122f48841364595c3341d823a95cc8661
hirwavestine <gilhirwa22@gmail.com>
2019-04-04 17:51:57 +0200

    another way to create a directory

--- file-creating-directories.js
+++ file-creating-directories.js
@@ -1,14 +1,2 @@
 const fs = require("fs");
-if (!fs.exists("views")) {
-  fs.mkdir("views", err => {
-    if (err) return err;
-    fs.writeFile(
-      "./views/new.html",
-      "This is a new directory and data",
-      err => {
-        if (err) return err;
-        console.log("Directory and file saved");
-      }
-    );
-  });
-}
+fs.mkdirSync("views");
