1e0927532c8f3d6392fb3fa3a2b272c7e0eae950
hirwavestine <gilhirwa22@gmail.com>
2019-04-09 04:58:59 +0200

    pull on json data

--- http-serving-json.js
+++ http-serving-json.js
@@ -9,14 +9,17 @@ http
       https.get(url, httpRes => {
         httpRes.on("data", data => {
           httpRes.setEncoding("utf8");
-          console.log(data);
+          serverRes.write(data);
         });
         httpRes.on("end", () => {
           serverRes.end();
           console.log("it is over!");
         });
       });
+    } else {
+      serverRes.writeHead(404, { "Content-Type": "text/plain" });
+      serverRes.end("404, could not find what you were looking for");
     }
   })
   .listen(4444);
-console.log("Our server is running!");
+console.log("Our server is listening");
