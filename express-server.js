5bdbf8f3b51fb90d133ee4b665f5d6b32695b4f8
hirwavestine <gilhirwa22@gmail.com>
2019-04-10 09:11:34 +0200

    create an other route /api

--- express-server.js
+++ express-server.js
@@ -5,6 +5,10 @@ let app = express();
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to our app</h1>");
 });
+//create an other route api
+app.get("/api", (req, res) => {
+  res.send("<h1>API PAGE</h1>");
+});
 
 app.listen(port);
 console.log("My app is running");
