34a609413e8d51c0503bf1a3871f9141e17e62f2
hirwavestine <gilhirwa22@gmail.com>
2019-04-10 09:03:42 +0200

    create a route and environment variable

--- express-server.js
+++ express-server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const port = process.env.PORT || 9999;
 let app = express();
-app.listen(9999);
-console.log("Our app is working!");
+//create a route
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to our app</h1>");
+});
+
+app.listen(port);
+console.log("My app is running");
