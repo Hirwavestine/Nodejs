95f4aabc23095932eef2ff23c8d3168d461751c7
hirwavestine <gilhirwa22@gmail.com>
2019-04-10 13:23:51 +0200

    submiting Post data with express and add comments

--- express-post-data.js
+++ express-post-data.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 let app = express();
+//these two bodyparser allow to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
