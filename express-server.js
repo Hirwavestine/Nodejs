d595fa6763057b146d765144bd367a066ba4ad0f
hirwavestine <gilhirwa22@gmail.com>
2019-04-10 09:30:13 +0200

    add express-server.js files only

--- express-server.js
+++ express-server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const port = process.env.PORT || 9999;
-let app = express();
-//create a route
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to our app</h1>");
-});
-//create an other route api
-app.get("/api", (req, res) => {
-  //res.send("<h1>API PAGE</h1>");
-  res.json({ name: "Hirwa" });
-});
 
-app.listen(port);
-console.log("My app is running");
+let app = express();
+
+app.listen(9999);
+
+console.log(`It's working`);
