cb7e74275ab88469e6d01a50c2333e7b962ad693
hirwavestine <gilhirwa22@gmail.com>
2019-04-11 10:34:11 +0200

    Sending a form data

--- websocket-client.js
+++ websocket-client.js
@@ -1,1 +1,8 @@
 const WS = new WebSocket("ws://localhost:3232");
+//getting data from the form
+document.forms[0].onsubmit = () => {
+  let input = document.getElementById("message");
+  console.log(input.value);
+  //send data to our webserver using an instance WS
+  WS.send(input.value);
+};
--- websocket-server.js
+++ websocket-server.js
@@ -2,5 +2,9 @@ const WebSocketServer = require("ws").Server;
 const WSS = new WebSocketServer({ port: 3232 });
 //listening on client
 WSS.on("connection", ws => {
+  //listen on message
+  ws.on("message", message => {
+    console.log(message);
+  });
   console.log("We are connected");
 });
